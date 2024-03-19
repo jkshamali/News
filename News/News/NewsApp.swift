0496b34d28ea76361a2acb1cf6bc3950b0d7b587
jkshamali <jkshamali2001@gmail.com>
2024-03-19 10:34:49 +0300

    first commit

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by dvp on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
